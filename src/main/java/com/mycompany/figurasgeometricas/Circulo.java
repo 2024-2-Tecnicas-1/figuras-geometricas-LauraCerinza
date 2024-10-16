1186df1b0e88bac548bc43b57110c1f856a06b4b
cualquieririzado <correo@correp.com>
2024-10-15 19:48:01 -0500

    FiguraGeometrica
    continuar

---
+++ src/main/java/com/mycompany/figurasgeometricas/Circulo.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.figurasgeometricas;
+
+public class Circulo extends FiguraGeometrica {
+private double radio;
+
+    public Circulo(String nombre, String color double radio) {
+        super (nombre, color);
+        this.radio = radio ;
+    }
+
+    }
